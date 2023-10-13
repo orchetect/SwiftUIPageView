b3917424378c5338f5b5832821d64ddbf943103e
Steffan Andrews <stef@orchetect.com>
2023-10-13 01:15:02 -0700

    Updated Example project

--- Example/SwiftUIPageView Example/ContentView.swift
+++ Example/SwiftUIPageView Example/ContentView.swift
@@ -9,27 +9,48 @@ import SwiftUI
 import SwiftUIPageView
 
 struct ContentView: View {
+    @State var pageIndex: Int = 0
+    @State var beginGestureDistance: BeginGestureDistance = .short
+    
     var body: some View {
-        PageViewReader { pageView in
+        VStack {
             PageView(
                 .horizontal,
                 alignment: .center,
                 pageLength: nil,
                 spacing: 10,
-                gestureMinimumDistance: .custom(0.1)
+                beginGestureDistance: beginGestureDistance,
+                index: $pageIndex
             ) {
                 TestView(text: "1")
                 TestView(text: "2")
                 TestView(text: "3")
             }
             .frame(width: 350)
-            .clipped()
-            .onAppear {
-                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
-                    pageView.moveToLast()
+            .opacityFadeMask(.horizontal, inset: 0.05)
+            
+            HStack {
+                Button("Go to Page 1") {
+                    pageIndex = 0
+                }
+                Button("Go to Page 3") {
+                    pageIndex = 2
+                }
+                Picker("Begin Distance:", selection: $beginGestureDistance) {
+                    ForEach(beginGestureDistanceOptions, id: \.self) {
+                        Text($0.name).tag($0)
+                    }
                 }
             }
         }
+        .padding()
+        .onChange(of: pageIndex) { newValue in
+            print("new page index:", newValue)
+        }
+    }
+    
+    var beginGestureDistanceOptions: [BeginGestureDistance] {
+        [.immediate, .short, .medium, .long, .never]
     }
 }
 
@@ -37,11 +58,28 @@ struct TestView: View {
     let text: String
     var body: some View {
         ZStack {
-            Color.blue
+            Color(hue: CGFloat.random(in: 0 ... 1), saturation: 1, brightness: 1)
             Text(text)
                 .font(.system(size: 48))
         }
         .frame(width: 300, height: 300)
+    }
+}
+
+extension View {
+    func opacityFadeMask(_ axis: Axis, inset: CGFloat = 0.2) -> some View {
+        mask {
+            LinearGradient(
+                stops: [
+                    .init(color: .clear, location: 0.0),
+                    .init(color: .black, location: max(inset, 0.0)),
+                    .init(color: .black, location: max(1.0 - inset, 1.0)),
+                    .init(color: .clear, location: 1.0)
+                ],
+                startPoint: axis == .horizontal ? .leading : .top,
+                endPoint: axis == .horizontal ? .trailing : .bottom
+            )
+        }
     }
 }
 
