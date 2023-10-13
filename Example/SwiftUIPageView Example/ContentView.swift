e1feffa1438f380c405820aed3bb4fe779c16496
Steffan Andrews <stef@orchetect.com>
2023-10-13 12:14:15 -0700

    Updated Example project

--- Example/SwiftUIPageView Example/ContentView.swift
+++ Example/SwiftUIPageView Example/ContentView.swift
@@ -15,21 +15,33 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            PageView(
-                .horizontal,
-                alignment: .center,
-                pageLength: nil,
-                spacing: 10,
-                beginGestureDistance: beginGestureDistance,
-                minGestureDistance: minGestureDistance,
-                index: $pageIndex
-            ) {
-                TestView(text: "1")
-                TestView(text: "2")
-                TestView(text: "3")
+            ZStack {
+                PageView(
+                    .horizontal,
+                    alignment: .center,
+                    pageLength: nil,
+                    spacing: 10,
+                    beginGestureDistance: beginGestureDistance,
+                    minGestureDistance: minGestureDistance,
+                    index: $pageIndex
+                ) {
+                    ForEach(pages) { $0 }
+                }
+                .frame(width: 350)
+                .opacityFadeMask(.horizontal, inset: 0.05)
             }
-            .frame(width: 350)
-            .opacityFadeMask(.horizontal, inset: 0.05)
+            
+            // Indicator dots
+            HStack(spacing: 8) {
+                ForEach(pages.indices, id: \.self) { index in
+                    Circle()
+                        .fill(pageIndex == index ? Color.primary : Color.secondary)
+                        .frame(width: 6, height: 6)
+                        .animation(.spring(), value: pageIndex == index)
+                        .onTapGesture { pageIndex = index }
+                }
+            }
+            .padding(.bottom)
             
             HStack {
                 Button("Go to Page 1") {
@@ -40,12 +52,12 @@ struct ContentView: View {
                 }
             }
             HStack {
-                Picker("Begin Distance:", selection: $beginGestureDistance) {
+                Picker("Begin Swipe Distance:", selection: $beginGestureDistance) {
                     ForEach(beginGestureDistanceOptions, id: \.self) {
                         Text($0.name).tag($0)
                     }
                 }
-                Picker("Min Distance:", selection: $minGestureDistance) {
+                Picker("Min Swipe Distance:", selection: $minGestureDistance) {
                     ForEach(minGestureDistanceOptions, id: \.self) {
                         Text($0.name).tag($0)
                     }
@@ -54,9 +66,15 @@ struct ContentView: View {
         }
         .padding()
         .onChange(of: pageIndex) { newValue in
-            print("new page index:", newValue)
+            print("New page index:", newValue)
         }
     }
+    
+    var pages = [
+        TestView(text: "1"),
+        TestView(text: "2"),
+        TestView(text: "3")
+    ]
     
     var beginGestureDistanceOptions: [BeginGestureDistance] {
         [.immediate, .short, .medium, .long, .never]
@@ -67,8 +85,10 @@ struct ContentView: View {
     }
 }
 
-struct TestView: View {
+struct TestView: View, Identifiable {
+    let id = UUID()
     let text: String
+    
     var body: some View {
         ZStack {
             Color(hue: CGFloat.random(in: 0 ... 1), saturation: 1, brightness: 1)
