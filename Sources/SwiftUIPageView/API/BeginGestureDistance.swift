c3d1b7ee452204e01a4725a0f241b23a2211505a
Steffan Andrews <stef@orchetect.com>
2023-10-13 01:01:45 -0700

    `BeginGestureDistance`: added `never` case

--- Sources/SwiftUIPageView/API/BeginGestureDistance.swift
+++ Sources/SwiftUIPageView/API/BeginGestureDistance.swift
@@ -9,11 +9,15 @@ import Foundation
 import CoreGraphics
 
 /// Minimum distance swipe distance before a swipe gesture begins.
-public enum BeginGestureDistance {
+public enum BeginGestureDistance: Hashable {
     case immediate
     case short
     case medium
     case long
+    
+    /// Disallow gestures from changing the page.
+    case never
+    
     case custom(CGFloat)
     
     var value: CGFloat {
@@ -22,7 +26,21 @@ public enum BeginGestureDistance {
         case .short: return 5
         case .medium: return 15
         case .long: return 31
+        case .never: return 100_000
         case .custom(let customDistance): return customDistance
+        }
+    }
+}
+
+extension BeginGestureDistance {
+    public var name: String {
+        switch self {
+        case .immediate: return "Immediate"
+        case .short: return "Short"
+        case .medium: return "Medium"
+        case .long: return "Long"
+        case .never: return "Never"
+        case .custom(let customDistance): return "\(customDistance) pts"
         }
     }
 }
