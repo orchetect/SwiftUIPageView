82d303383785e2ceb761dcbcdf95a81bbc2a4cf3
Steffan Andrews <stef@orchetect.com>
2023-10-17 21:18:55 -0700

    Package cleanup

--- Package.swift
+++ Package.swift
@@ -4,21 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUIPageView",
-    platforms: [
-        .iOS(.v14),
-        .watchOS(.v7),
-        .macOS(.v11),
-    ],
-    products: [
-        .library(
-            name: "SwiftUIPageView",
-            targets: ["SwiftUIPageView"]),
-    ],
-    dependencies: [
-    ],
-    targets: [
-        .target(
-            name: "SwiftUIPageView",
-            dependencies: [])
-    ]
+    platforms: [.iOS(.v14), .watchOS(.v7), .macOS(.v11)],
+    products: [.library(name: "SwiftUIPageView", targets: ["SwiftUIPageView"])],
+    targets: [.target(name: "SwiftUIPageView")]
 )
